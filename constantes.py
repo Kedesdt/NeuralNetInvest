9847111376262c4bb7da73de1317bff77ccbcef4
Kedes Dias Torres <kdtorres@band.com.br>
2023-10-03 08:41:51 -0300

    correção em um comentario

--- constantes.py
+++ constantes.py
@@ -2,7 +2,7 @@
 
 IBOV_JAN2022 = 104700
 IBOV_JAN2023 = 109900
-QUANTIDADE = 20 #Quantidade de redes neurais treinadas
+QUANTIDADE = 20 
 QUANTIDADEDEREPETICOES = 20 # quantidade de vezes em que o programa será reiniciado e refazer o teino
 QUANTIDADE_DE_ACOES = 10
 
@@ -13,7 +13,7 @@ DITES = '2022-01-02'
 DFTES = '2023-01-02'
 
 
-NUMERODEREDES = 5
+NUMERODEREDES = 5 #Quantidade de redes neurais treinadas
 EPOCAS = 500
 MINIBATCH = 30
 TAXA = 0.001
